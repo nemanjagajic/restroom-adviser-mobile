6e81e700efa9f7b4696ddfc88b2a6904c3bdd403
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-01-27 16:24:16 +0100

    added all to .env

--- config/index.js
+++ config/index.js
@@ -1,9 +1,13 @@
-import { FACEBOOK_APP_ID, ANDROID_GOOGLE_CLIENT_ID } from 'react-native-dotenv';
+import {
+  API_BASE_URL,
+  IMAGE_BASE_URL,
+  FACEBOOK_APP_ID,
+  ANDROID_GOOGLE_CLIENT_ID
+} from 'react-native-dotenv';
 
 const config = {
-  API_BASE_URL: 'http://67537f1a.ngrok.io/api',
-  IMAGE_BASE_URL: 'http://67537f1a.ngrok.io/storage',
-
+  API_BASE_URL,
+  IMAGE_BASE_URL,
   FACEBOOK_APP_ID,
   ANDROID_GOOGLE_CLIENT_ID
 };
